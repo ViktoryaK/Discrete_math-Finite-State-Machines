21f40bac4e0a7e938493dbbfcfe75e820597170b
ViktoryaK <96166901+ViktoryaK@users.noreply.github.com>
2022-05-26 00:02:29 +0300

    Delete visualisation directory

--- visualisation/graph_vis.py
+++
@@ -1,9 +0,0 @@
-from graphviz import Digraph
-dot = Digraph(comment='A simple Graph')
-dot.node('A', 'Cloudy')
-dot.node('B', 'Sunny')
-dot.node('C', 'Rainy')
-dot.edges(['AB', 'AC'])
-dot.edge('B', 'C', constraint='false')
-dot.format = 'png'
-dot.render('my_graph', view=False)
